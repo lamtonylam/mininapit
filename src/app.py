495361a605fa3fa96726943c0a4e553ce648b1b8
tiikerikakku <146967003+tiikerikakku@users.noreply.github.com>
2024-11-21 14:52:12 +0200

    cleaning

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
-from flask import redirect, render_template, request, jsonify, flash
+from flask import redirect, render_template, request, flash
 from db_helper import reset_db
-from repositories.citation_repository import get_citations, create_todo, set_done, create_citation
+from repositories.citation_repository import get_citations, create_citation
 from config import app, test_env
 from util import validate_todo
 
@@ -26,34 +26,7 @@ def create_new():
 
     create_citation(key, author, title, journal, year, volume, pages)
 
-    citations = get_citations()
-
-    return render_template('index.html', citations=citations)
-
-
-
-
-
-# @app.route("/new_todo")
-# def new():
-#     return render_template("new_todo.html")
-
-# @app.route("/create_todo", methods=["POST"])
-# def todo_creation():
-#     content = request.form.get("content")
-# 
-#     try:
-#         validate_todo(content)
-#         create_todo(content)
-#         return redirect("/")
-#     except Exception as error:
-#         flash(str(error))
-#         return  redirect("/new_todo")
-
-# @app.route("/toggle_todo/<todo_id>", methods=["POST"])
-# def toggle_todo(todo_id):
-#     set_done(todo_id)
-#     return redirect("/")
+    return redirect('/')
 
 if test_env:
     @app.route('/reset_db')
