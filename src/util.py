a0c8fd91237ccdfa73fe4524c6ee0f23a390101c
tiikerikakku <146967003+tiikerikakku@users.noreply.github.com>
2024-11-27 18:50:01 +0200

    more trash removed

--- src/util.py
+++ src/util.py
@@ -1,13 +1,3 @@
-class UserInputError(Exception):
-    pass
-
-def validate_todo(content):
-    if len(content) < 5:
-        raise UserInputError('Todo content length must be greater than 4')
-
-    if len(content) > 100:
-        raise UserInputError('Todo content length must be smaller than 100')
-
 def string_or_empty_string(s):
     if s is None:
         return ''
